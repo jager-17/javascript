fe8ffe7a6e5d72e4cf9f20b5fbcff3f7baa31d18
Kaushal Jangir <kaushaljangir1718@gmail.com>
2025-08-02 00:18:17 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 1357
+let accountEmail = "Kaushal @gmail.com"
+var accountPasssword = "12345"
+accountCity = "jaipur"
+var state ;
+
+// accountID = 3457  // not allowed
+
+accountEmail = "ght@gmail.com"
+accountPasssword = "77777"
+accountCity = "goa"
+
+console.log(accountID)
+
+console.table([accountID, accountEmail, accountPasssword, accountCity, state])

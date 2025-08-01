8658e4588b3e8b8ff225f19511e80fe9f5b55124
Kaushal Jangir <kaushaljangir1718@gmail.com>
2025-08-02 00:19:09 +0530

    Create 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(3 + 3)
+
+console.log("kasuhal")
+
+let name = "kj"
+let age = 19
+let isloogedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "kaushal"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined

29b8de177a4ea4c3582fe20e1248a3585b36c295
Kaushal Jangir <kaushaljangir1718@gmail.com>
2025-08-09 05:58:15 +0000

    both

---
+++ 03_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}            // switch case if ishme apan break hata de then after matching jo bhi. case hoga woh print hojayega except default

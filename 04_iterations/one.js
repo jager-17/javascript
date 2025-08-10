39f651014df56d18be3d5dd8e9c349fc16d1539a
Kaushal Jangir <kaushaljangir1718@gmail.com>
2025-08-10 06:04:46 +0000

    for loop

--- 04_iterations/one.js
+++ 04_iterations/one.js
@@ -37,15 +37,46 @@ thala for a reason
 8
 9 */
 
-for (let i = 0; i < 10; i++) {
-     console.log(`outer loop value: ${i}`);
-     for (let j = 0; j < 10; j++) {
-          // console.log(`inner loop value: ${j} and inner loop ${i}`);
-          console.log(i + '*' + j + '=' + i*j);  // table 0*1 = 0 ese
+// for (let i = 0; i < 10; i++) {
+//      console.log(`outer loop value: ${i}`);
+//      for (let j = 0; j < 10; j++) {
+//           // console.log(`inner loop value: ${j} and inner loop ${i}`);
+//           console.log(i + '*' + j + '=' + i*j);  // table 0*1 = 0 ese
           
-     }
-} 
+//      }
+// } 
 
 // outer loop 0 par aayega fir inner loop 10 baar chalega fir outer loop 1 par aayega fir wapis inner loop 10 baar chalega
 
+// let myArray = ["flash", "batman", "thor"]
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//      const element = myArray[index];
+//      console.log(element)
+     
+// }    
+
+/* flash
+batman
+thor */
+
+// break and continue
+
+for (let i = 1; i <= 20; i++) {
+     if (i == 5) {
+          console.log('detected 5');
+          break                               // 4 tak print hoga bas
+     }
+     console.log(`value of i is ${i}`);
+}
+
+
+
+for (let i = 1; i <= 20; i++) {
+     if (i == 5) {
+          console.log('detected 5');
+          continue                               // sab print hoga 5 k alawa
+     }
+     console.log(`value of i is ${i}`);
+}
 
